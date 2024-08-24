144fd18ab2586ec5a5d63ded58da19f8c3a52ce9
Gleb Shershnev <gleb2000405@outlook.com>
2024-08-24 18:55:12 +0300

    Update

--- MSTesseract/src/app.js
+++ MSTesseract/src/app.js
@@ -1,6 +1,9 @@
 import Tesseract from 'tesseract.js';
 import { preload, getPath } from './utils/preload.js';
 import { logger } from './utils/logger.js';
+import { keywords } from './keywords.js';
+
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz6TDirBGr8Qf2uGXcNcAhrDlRH2jGZPsBYY7Yi8-HTzdMSXp_dooGpfnGjANv77N4Qbw/exec';
 
 const input = getPath('./assets/input.jpg');
 const output = getPath('./assets/output.jpg');
@@ -8,30 +11,22 @@ const output = getPath('./assets/output.jpg');
 const run = async (input) => {
     await preload(input, output);
 
-    return Tesseract.recognize(
-        output,
-        'rus',
-        {
+    try {
+        const { data: { text } } = await Tesseract.recognize(output, 'rus', {
             logger: info => logger.info(info.status)
-        }
-    ).then(({ data: { text } }) => {
-        let ignoreNext = false;
-        console.log(text);
-
-        const lines = text.split('\n');
+        });
 
         const amounts = [];
-
         const priceWithRRegex = /[+-]?\d+(?:[\s,]\d{3})*(?:[.,]\d+)?\s*Р/g;
 
-        for (const line of lines) {
-            if (line.includes('Вчера') || line.includes('Сегодня')) {
+        let ignoreNext = false;
+        text.split('\n').forEach(line => {
+            if (keywords.some(keyword => line.includes(keyword))) {
                 ignoreNext = true;
             }
 
             if (!ignoreNext) {
                 const priceMatches = line.match(priceWithRRegex);
-
                 if (priceMatches) {
                     priceMatches.forEach(priceMatch => {
                         let price = priceMatch
@@ -55,20 +50,35 @@ const run = async (input) => {
             if (ignoreNext) {
                 ignoreNext = false;
             }
+        });
+
+        const username = 'gleb';
+        const date = new Date().toLocaleDateString('ru-RU');
+        const article = 'Сбербанк';
+
+        for (const amount of amounts) {
+            const newRow = {
+                username,
+                date,
+                amount,
+                article
+            };
+
+            await fetch(WEB_APP_URL, {
+                redirect: "follow",
+                method: "POST",
+                body: JSON.stringify(newRow),
+                headers: {
+                    "Content-Type": "text/plain;charset=utf-8",
+                },
+            });
         }
-
-        const total = amounts.reduce((acc, curr) => acc + curr, 0);
-
-        return {
-            amounts,
-            total
-        };
-    }).catch(error => {
+    } catch (error) {
         logger.info(error);
         throw error;
-    });
-}
+    }
+};
 
-run(input).then(result => {
-    logger.info(result, 'Result: ');
+run(input).then(() => {
+    logger.info('OK!');
 });
